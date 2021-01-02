6ced5b8ded8d55be46f92d1efd7bd9ee07c344b2
Saksham Aggarwal <75024792+SakshamAggarwal2812@users.noreply.github.com>
2021-01-02 18:34:26 +0530

    Add files via upload

---
+++ ReadFileMain.java
@@ -0,0 +1,11 @@
+package swing1;
+
+import javax.swing.JFrame;
+//import ReadFile.ReadFile;
+
+public class ReadFileMain{
+	public static void main(String[] args){
+		ReadFile dude= new ReadFile();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
